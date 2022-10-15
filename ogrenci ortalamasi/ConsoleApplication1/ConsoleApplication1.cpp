9608c08fee77a2f7cb9ccbb11bc3d71038ea3e05
MusaSSH <97892949+MusaSSH@users.noreply.github.com>
2022-10-15 23:10:21 +0300

    ogrenci ortalamasi

---
+++ ogrenci ortalamasi/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    int ogrenciSayisi = 0;
+    int toplamNot = 0;
+
+    std::cout << "Ogrenci sayisini girin" << std::endl;
+    std::cin >> ogrenciSayisi;
+    // std::cout << "DEBUG: Ogrenci sayisi " << ogrenciSayisi << " olarak belirlendi." << std::endl;
+
+    int *notlar = new int[ogrenciSayisi];
+
+    std::cout << "Sirasiyla ogrencilerin notlarini giriniz." << std::endl;
+
+    for (int i = 0; i < ogrenciSayisi; i++) {
+        std::cout << i + 1 << ". ogrencinin notunu giriniz." << std::endl;
+        std::cin >> notlar[i];
+        // std::cout << "DEBUG: " << i + 1 << ". ogrencinin notu " << notlar[i] << " olarak girildi." << std::endl;
+    }
+
+    for (int i = 0; i < ogrenciSayisi; i++) {
+        toplamNot += notlar[i];
+        // std::cout << "DEBUG: " << i + 1 << ". ogrencinin notu toplam nota eklendi." << std::endl;
+    }
+
+    // std::cout << "DEBUG: " << ogrenciSayisi << " ogrencinin toplam notu " << toplamNot << std::endl;
+
+    std::cout << ogrenciSayisi << " ogrencinin not ortalamasi:" << (float) toplamNot / (float) ogrenciSayisi << std::endl;
+}
